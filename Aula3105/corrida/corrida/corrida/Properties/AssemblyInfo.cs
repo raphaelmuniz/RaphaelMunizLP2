bf61232e04f290179423c47d121969a9841ee0cd
raphaelmuniz <raphaeldasilv@hotmail.com>
2017-06-10 20:46:29 -0300

    cc

--- Aula3105/corrida/corrida/corrida/Properties/AssemblyInfo.cs
+++ Aula3105/corrida/corrida/corrida/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("corrida")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("corrida")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5fbcada1-89e9-4573-b0b0-ecf714b86474")]
+[assembly: Guid("ed7de6af-f4ce-43f2-ae8e-fde36fe4986a")]
 
 // Version information for an assembly consists of the following four values:
 //
