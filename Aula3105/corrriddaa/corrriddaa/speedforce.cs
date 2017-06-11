032b57ece5c539ca207a8a5467da8268378f95bd
raphaelmuniz <raphaeldasilv@hotmail.com>
2017-06-10 21:06:08 -0300

    xx

---
+++ Aula3105/corrriddaa/corrriddaa/speedforce.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace corrriddaa
+{
+    class speedforce
+    {
+    
+        public string nome;
+        public double tempo1;
+        public double tempo2;
+        public double tempo3;
+
+        public double Tempo()
+        {
+            double[] temp = new double[3] { tempo1, tempo2, tempo3 };
+            Array.Sort(temp);
+            return temp[0] + temp[1];
+    }
+}
