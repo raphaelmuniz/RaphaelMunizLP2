90620a56788d1b22ff3a8b5a82ec3317ef767f17
raphaelmuniz <raphaeldasilv@hotmail.com>
2017-07-11 21:32:22 -0300

    completo

---
+++ 2c/2-LivrosHermione/2-LivrosHermione/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2_LivrosHermione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            int pagporhoras, pagpordia,n;
+            Console.WriteLine("numero de livros");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("paginas por horas");
+            pagporhoras = int.Parse(Console.ReadLine());
+            Console.WriteLine("Paginas por dias");
+            pagpordia = int.Parse(Console.ReadLine());
+            
+            livros[] livro = new livros[n];
+            for (int i = 0; i <= n; i++)
+            {
+                livro[i] = new livros();
+                Console.WriteLine("titulo");
+                livro[i].titulo = Console.ReadLine();
+                Console.WriteLine("paginas");
+                livro[i].pag = int.Parse(Console.ReadLine());
+                Console.WriteLine(" dias para delvolver ");
+                livro[i].diasrestantes = int.Parse(Console.ReadLine());
+                livro[i].cal(pagpordia, pagporhoras);
+
+
+                if (livro[i].cal(pagpordia, pagporhoras) > livro[i].diasrestantes)
+                {
+                    
+                    Console.WriteLine("Esse livro impossivel de ler :{0} ", livro[i].titulo);
+                }
+                else
+                {
+                    Console.WriteLine("Dias necessario{0}", livro[i].cal(pagpordia, pagporhoras));
+                }
+                
+            }
+             
+        
+        }
+    }
+}
---
+++ 2c/2-LivrosHermione/2-LivrosHermione/livros.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2_LivrosHermione
+{
+    class livros
+    {
+        public string titulo;
+        public int pag;
+        public int diasrestantes;
+
+        public int cal(int pagporhoras, int pagpordia)
+        {
+            int Paglidas = pagpordia * pagporhoras;
+            int leitura = pag / Paglidas;
+            return leitura;
+
+        }
+    }
+}
