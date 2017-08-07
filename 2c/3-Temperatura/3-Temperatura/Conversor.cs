ac0574e392b97a2ce5c49ab0c981e9b3945b3a65
raphaelmuniz <raphaeldasilv@hotmail.com>
2017-08-07 18:55:10 -0300

    terminei

---
+++ 2c/3-Temperatura/3-Temperatura/Conversor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3_Temperatura
+{
+    class Conversor
+    {
+        public double celsus, fahrenheit, kelvin;
+
+        public double converemfar(double temp)
+        { 
+            return fahrenheit = (celsus * 1.8) + 32;
+        }
+
+        public double converemkel(double temp)
+        {
+            return kelvin = celsus + 273;
+        }
+    }
+}
---
+++ 2c/3-Temperatura/3-Temperatura/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3_Temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tempc;
+            string tempdif;
+
+            Console.WriteLine("Informe a tempeatura");
+            tempc = double.Parse(Console.ReadLine());
+
+            Conversor Conversão = new Conversor();
+
+            Console.WriteLine("Digite para qual você quer converter");
+            tempdif = Console.ReadLine();
+            Conversão.celsus = tempc;
+            if (tempdif == "k")
+            {
+                Console.WriteLine(Conversão.converemkel(Conversão.kelvin));
+
+            }
+            else
+            {
+                Console.WriteLine(Conversão.converemkel(Conversão.fahrenheit));
+            }
+        }
+    }
+}
