8970bc7a1511079cff50f024aa822e0ba1391d28
raphaelmuniz <raphaeldasilv@hotmail.com>
2017-08-20 21:37:55 -0300

    aula do 16

---
+++ 2c/05-Produtos/05-Produtos/Produto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05_Produtos
+{
+    class Produto
+    {
+        private int id;
+        public int Id 
+        {
+            get { return id; }
+        }
+        public double preco { get; set; }
+      
+        private string nome;
+        public string Nome {
+            get { return nome; }
+        }
+        private int quantidade;
+        public int Quant { get { return quantidade; } }
+
+
+        public Produto (int id ,string nome , double preco)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.preco = preco;
+            
+        }
+        public void Receber(int qtd)
+        {
+            
+                this.quantidade += qtd;
+            
+        }
+         public void Retirada (int qtd)
+        {
+             if (this.Quant >=qtd)
+             {
+                 this.quantidade -= qtd;
+             }
+             else
+             {
+                 throw new Exception();
+
+             } 
+
+        }
+        public string Imprimir()
+         {
+             return string.Format("{0}, {1}, {2:0.00}", id, nome, preco);
+         }
+    }
+
+    }
+
---
+++ 2c/05-Produtos/05-Produtos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05_Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto(1, "Sabonete Francis 60g", 1.49);
+            p.Receber(100);
+            p.Retirada(20);
+            Console.WriteLine(p.Imprimir());
+            Console.ReadLine();
+
+
+        }
+    }
+}
