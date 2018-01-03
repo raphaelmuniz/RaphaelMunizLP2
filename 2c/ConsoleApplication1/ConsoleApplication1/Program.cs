44ea6f555dd7763e3e699f89c9db6fff23aaa04f
raphaelmuniz <raphaeldasilv@hotmail.com>
2018-01-03 16:28:11 -0200

    AQUI HIGA

---
+++ 2c/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        
+           
+
+            string nomel,autor,numerol,genero,classificacao,nomec,senha,email,endereco,cpf,data,;
+            int idL,idC,R;
+            int P = 0;
+
+            Console.WriteLine("O que deseja fazer?\n1-Efetuar o login;\n2-Fazer o regitro;\n3-Sair;"); // entrada//
+            R = int.Parse(Console.ReadLine());
+
+            if (R == 1)//login incompleto //
+            {
+                Console.WriteLine("senha");
+                senha = Console.ReadLine();
+                Console.WriteLine("email");
+                email = Console.ReadLine();
+                while (P < 4) // AÇÕES DEPOIS DO LOGIN//
+                {
+                    Console.WriteLine("O que voce deseja fazer? \n1-Alugue;\n2-Comprar;\n3-Apenas uma consulta;\n4-Sair;");
+                    P = int.Parse(Console.ReadLine());
+
+                    if (P == 1)
+                    {
+                        // alugueu do livro //
+                    }
+                    else if (P == 2)
+                    {
+                        // compra do livro//
+                    }
+                    else if (P == 3)
+                    {
+                        // consulta //
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                
+            }
+            else if (R == 2) // O registro da pessoa//
+            {
+                Console.WriteLine("\nNome:");
+                nomec = Console.ReadLine();
+
+                Console.WriteLine("\nCPF:");
+                cpf = Console.ReadLine();
+
+                Console.WriteLine("\nNúmero de registro:");
+                idC = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nE-mail:");
+                email = Console.ReadLine();
+
+                Console.WriteLine("\nData de nascimento (dd/mm/aaaa:");
+                data = Console.ReadLine();
+
+                Console.WriteLine("\nSenha:");
+                senha = Console.ReadLine();
+
+                Console.WriteLine("\n Endereco:");
+                endereco = Console.ReadLine();
+
+                Registro f = new Registro(nomec, cpf, idC, email, data, senha, endereco);
+                Console.WriteLine(f.cadastro(nomec, cpf, idC, email, data, senha, endereco));//conexão com banco de dados está na classe registro//
+                while (P < 4 )// AÇÕES DEPOIS DO LOGIN//
+                { Console.WriteLine("O que voce deseja fazer? \n1-Alugue;\n2-Comprar;\n3-Apenas uma consulta;\n4-Sair;"); 
+                P =int.Parse(Console.ReadLine());
+
+                if (P == 1)
+                { 
+                // alugueu do livro //
+                }
+                else if (P == 2)
+                {
+                    // compra do livro//
+                }
+                else if (P == 3)
+                {
+                    // consulta //
+
+                }
+                else
+                {
+                    break; //sair//
+                }
+                }
+            }
+            else
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ 2c/ConsoleApplication1/ConsoleApplication1/Registro.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+namespace ConsoleApplication1
+{
+    class Registro
+    {
+        public int Idc { get; set; }
+        public string Data { get; set; }
+        public string Nome { get; set; }
+        public string CPF  { get; set; }
+        public string Email{ get; set; }
+        public string Senha{ get; set; }
+        public string Endereco{ get; set; }
+        
+        SqlCommand cmd = new SqlCommand()
+        {
+            Connection = new SqlConnection("Data Source=localhost; Initial Catalog=BancoDeDados; Integrated Security=SSPI;"),
+        };
+       
+        
+        public Registro(string nome, string cpf, int idc, string email, string data, string senha, string endereco)
+        {
+            this.Nome = nome;
+            this.Data = data;
+            this.Idc = idc;
+            this.CPF = cpf;
+            this.Email = email;
+            this.Senha = senha;
+            this.Endereco = endereco;
+                        
+        }
+        public string cadastro(string nome, string cpf, int idc, string email, string data, string senha, string endereco)
+        {
+              
+              cmd.CommandText = String.Format(@"INSERT
+                                              INTO cliente
+                                              VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}');", Nome, Data, Idc, CPF, Email, Senha, Endereco);
+
+              return String.Format("O seu registro foi concluindo:", nome);
+
+          
+        }
+        public string Login(string nome, string email)
+        { 
+        
+        
+        }
+
+    }
+}
